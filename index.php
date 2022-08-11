4c37e0a298a05f23d67109223c596b0114b6624b
kayyhan <cuvillier.remy@hotmail.fr>
2022-08-11 20:38:32 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css" />
+    <title>Mettre un titre</title>
+</head>
+
+<body class="o-body">
+    <div class="o-header">
+        <div class="o-navigation">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+    <div class="o-main">
+        <h1>super</h1>
+    </div>
+</body>
+
+</html>
